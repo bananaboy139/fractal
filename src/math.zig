4f55d8492b9fa6c2abbe46d58dcf894af355de1f
Batman <matteob319@gmail.com>
2022-09-08 16:59:32 +0200

    init

---
+++ src/math.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const math = std.math;
+const cmath = math.complex;
+const Complex = cmath.Complex;
+//don't use outside of check
+fn check_error(guess: Complex(f64), ans: Complex(f64)) f64 {
+	return cmath.abs(ans.sub(guess).div(ans)); //don't know how div works 
+}
+///check error 
+									//(err, ans)
+fn check(value: Complex(f64)) .{f64, Complex(f64)} {
+	const a1 = Complex(f64).init(1, 0);
+	const e1 = check_error(value, a1);
+
+	const a2 = euler(1, (2*cmath.pi)/3);
+	const e2 = check_error(value, a2);
+
+	const a3 = euler(1, (4*cmath.pi)/3);
+	const e3 = check_error(value, a3);
+	//really cool sorting 
+	if (e1 < e2) {
+		if (e1 < e3) {
+			return .{e1, a1};
+		} else {
+			return .{e3, a3};
+		}
+	} else if (e2 < e3) {
+		return .{e2, a2};
+	} else {
+		return .{e3, a3};
+	}
+}
+///translate euler form to cartisien 
+fn euler(radius: f32, magnitude: f32) Complex(f64) {
+	var r: f32 = math.cos(magnitude) * radius;
+	var i: f32 = math.sin(magnitude) * radius;
+	return Complex(f64).init(r, i);
+}
+///newton optimized root finding function
+fn optimized(n: Complex(f64)) Complex(f64) {
+	var n_2 = n.mul(n);
+	var n_3 = n_2.mul(n);
+	return (2*(n_3)+1)/(3*(n_2));
+}
+
+fn draw(max_x: u32, max_y: u32)  {
+//	var max_x: u32 = graphics.getmaxx();
+//	var max_y: u32 = graphics.getmaxy();
+//	graphics.rectangle(0, 0, max_x, max_y);
+	const scale = 10;
+	const acc_err = 0.01;
+	const max_trial = 1_000;
+	var x: u32 = 1;
+	while (x < max_x/scale) {
+		var y = 1;
+		while (y < max_y/scale) {
+			var trial: u16 = 0;
+			var z = Complex(f64).init(x, y);
+			while (check(z)[0] > acc_err and trial < max_trial) {
+				z = optimized(z);
+				trial += 1;
+			}
+			//graphics.putpixel(x*scale, y*scale, trial);
+			y += 1/scale;
+		}
+		x += 1/scale;
+	}
+}
